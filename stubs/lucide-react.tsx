1a9c7508193b388ae76b1a2b12f7f422b4fe6f84
celsoneves32 <celsoneves20@gmail.com>
2025-11-06 09:13:10 +0100

    Create lucide-react.tsx

---
+++ stubs/lucide-react.tsx
@@ -0,0 +1,6 @@
+// Fallback leve para evitar falhas de build se algum import de 'lucide-react' sobrar.
+export const Phone = (props: any) => <span {...props} />;
+export const Mail = (props: any) => <span {...props} />;
+export const MessageSquare = (props: any) => <span {...props} />;
+export const ArrowUp = (props: any) => <span {...props} />;
+export default {};
