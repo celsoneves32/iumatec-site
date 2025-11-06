14059cc903877c28862d42cded68f5c9211268ca
celsoneves32 <celsoneves20@gmail.com>
2025-11-06 19:09:13 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
+// next.config.mjs
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config) => {
+    // força qualquer import "lucide-react" a usar o stub local
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "lucide-react": path.resolve(process.cwd(), "stubs/lucide-react.tsx"),
+    };
+    return config;
+  },
 };
+
 export default nextConfig;
