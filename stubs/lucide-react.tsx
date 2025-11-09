508659496e948dace3d4b123e62c8b2c87cb5ca6
celsoneves32 <celsoneves20@gmail.com>
2025-11-09 21:50:27 +0100

    Update lucide-react.tsx

--- stubs/lucide-react.tsx
+++ stubs/lucide-react.tsx
@@ -1,6 +1,5 @@
-// stubs/lucide-react.tsx
-export const Phone = (p: any) => <span {...p} />;
-export const Mail = (p: any) => <span {...p} />;
-export const MessageSquare = (p: any) => <span {...p} />;
-export const ArrowUp = (p: any) => <span {...p} />;
+export const Phone = (p:any) => <span {...p} />;
+export const Mail = (p:any) => <span {...p} />;
+export const MessageSquare = (p:any) => <span {...p} />;
+export const ArrowUp = (p:any) => <span {...p} />;
 export default {};
