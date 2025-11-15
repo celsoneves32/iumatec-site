a790ae200b51464eb034e649fc4f542581cbbc3d
celsoneves32 <celsoneves20@gmail.com>
2025-11-15 21:52:53 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,16 +2,15 @@
 module.exports = {
   siteUrl: "https://iumatec.ch",
   generateRobotsTxt: true,
-  sitemapSize: 5000,
   changefreq: "weekly",
-  priority: 0.7,
-  exclude: ["/404", "/500"],
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: "*", allow: "/" },
-    ],
-    additionalSitemaps: [
-      "https://iumatec.ch/sitemap.xml",
-    ],
+  sitemapSize: 5000,
+  exclude: ["/api/*"],
+  transform: async (config, route) => {
+    return {
+      loc: route,
+      changefreq: "weekly",
+      priority: 0.8,
+      lastmod: new Date().toISOString(),
+    };
   },
 };
