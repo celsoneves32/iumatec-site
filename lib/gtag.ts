474c105fa63f73bc2aad4a823ae36b1d0d346e60
celsoneves32 <celsoneves20@gmail.com>
2025-11-16 23:53:45 +0100

    Update gtag.ts

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,3 @@
-// lib/gtag.ts
 export const gtagEvent = ({ action, params }: any) => {
   if (typeof window !== "undefined" && (window as any).gtag) {
     (window as any).gtag("event", action, params);
