fa6a2e526f50142d49beeeed5bc4e0027a3756aa
celsoneves32 <celsoneves20@gmail.com>
2025-11-18 12:36:34 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
+  // URL base do teu site em produção
   siteUrl: "https://iumatec.ch",
+
+  // Gera automaticamente /sitemap.xml e /robots.txt
   generateRobotsTxt: true,
-  sitemapSize: 5000,
+
+  // Opcional: tamanho máximo de cada sitemap
+  sitemapSize: 7000,
+
+  // Valores genéricos de SEO
+  changefreq: "daily",
+  priority: 0.7,
+
+  // Rotas que não devem aparecer no sitemap
+  exclude: ["/api/*", "/_next/*"],
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/admin"],
+      },
+    ],
+  },
 };
