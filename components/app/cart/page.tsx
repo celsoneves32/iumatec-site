5a5b40e21efb583c521da97b3547c8d62904d8fd
celsoneves32 <celsoneves20@gmail.com>
2025-11-19 23:32:59 +0100

    Create page.tsx

---
+++ components/app/cart/page.tsx
@@ -0,0 +1,9 @@
+import CartPageClient from "@/components/CartPageClient";
+
+export const metadata = {
+  title: "Warenkorb | IUMATEC",
+};
+
+export default function CartPage() {
+  return <CartPageClient />;
+}
