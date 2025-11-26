64b24b233bed13bf30743121817d8a3b6af949a7
celsoneves32 <celsoneves20@gmail.com>
2025-11-26 22:58:03 +0100

    Create supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,8 @@
+// lib/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Cliente para usar no browser (login, conta, etc.)
+export const supabaseBrowser = createClient(supabaseUrl, supabaseAnonKey);
