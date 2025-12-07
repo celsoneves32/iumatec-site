3f74145b76680f4ec687041b8ca840b4886aabbf
celsoneves32 <celsoneves20@gmail.com>
2025-12-07 22:20:23 +0100

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,15 +1,14 @@
 // app/checkout/page.tsx
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import CheckoutClient from "./CheckoutClient";
 
 export default async function CheckoutPage() {
   const session = await getServerSession(authOptions);
 
-  // Nur eingeloggte Nutzer dürfen zur Kasse
   if (!session) {
-    redirect(`/login?from=/checkout`);
+    return redirect("/login?from=/checkout");
   }
 
   return <CheckoutClient />;
