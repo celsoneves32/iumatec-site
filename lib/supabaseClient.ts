a427ff590d44221484a15987584654213e6c9e01
celsoneves32 <celsoneves20@gmail.com>
2025-12-10 18:37:03 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,9 @@
 // lib/supabaseClient.ts
+"use client";
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Cliente para usar no browser (login, conta, etc.)
-export const supabaseBrowser = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
