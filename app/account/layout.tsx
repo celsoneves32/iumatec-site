f5b6266e31245574b13878a977a75b71074990be
Celso Neves <celso_32@hotmail.com>
2026-02-08 20:16:40 +0100

    chore: remove /account (shopify-only)

--- app/account/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
-
-export default async function AccountLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(authOptions);
-  if (!session) redirect("/login?from=/account");
-  return <>{children}</>;
-}
