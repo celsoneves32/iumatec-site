8a98155965332e0141bc3a0cbd7c333305de7141
celsoneves32 <celsoneves20@gmail.com>
2026-02-14 22:32:20 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
-// app/layout.tsx
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
+import { FavoritesProvider } from "@/context/FavoritesContext";
 import SiteHeader from "@/components/SiteHeader";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de-CH">
       <body>
-        <CartProvider>
-          <SiteHeader />
-          {children}
-        </CartProvider>
+        <FavoritesProvider>
+          <CartProvider>
+            <SiteHeader />
+            {children}
+          </CartProvider>
+        </FavoritesProvider>
       </body>
     </html>
   );
