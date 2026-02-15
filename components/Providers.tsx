3ec6a5026b60ffa96d0cf6c17707b8f1bb16136d
celsoneves32 <celsoneves20@gmail.com>
2026-02-15 19:37:59 +0100

    Update Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { CartProvider } from "@/context/CartContext";
 
 export default function Providers({ children }: { children: ReactNode }) {
