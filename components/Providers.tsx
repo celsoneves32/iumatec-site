ee42895357d58fae4a6ea10efee45d9e7526bd51
celsoneves32 <celsoneves20@gmail.com>
2026-02-15 17:20:32 +0100

    Update Providers.tsx

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,14 +1,8 @@
-// components/Providers.tsx
 "use client";
 
 import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "@/context/CartContext";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return (
-    <SessionProvider>
-      <CartProvider>{children}</CartProvider>
-    </SessionProvider>
-  );
+  return <CartProvider>{children}</CartProvider>;
 }
