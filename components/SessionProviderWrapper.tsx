99a85d913f152e224e89114aa330413db6019963
celsoneves32 <celsoneves20@gmail.com>
2026-02-15 18:06:27 +0100

    Delete components/SessionProviderWrapper.tsx

--- components/SessionProviderWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ReactNode } from "react";
-import { CartProvider } from "@/context/CartContext";
-
-export default function Providers({ children }: { children: ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}
