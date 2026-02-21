c2a580321d0910a9a70c3a498f9eb43ab39ebc03
celsoneves32 <celsoneves20@gmail.com>
2026-02-21 09:15:40 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./context/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "rgb(225 29 72)",     // #e11d48
+          dark: "rgb(190 18 60)",        // #be123c
+        },
+        "brand-red": "rgb(225 29 72)",   // se usas hover:text-brand-red no footer
+      },
+    },
   },
   plugins: [],
 };
