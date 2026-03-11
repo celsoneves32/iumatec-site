34d762907e5b2f43ae9ec9fd8535403b9830e2ad
celsoneves32 <celsoneves20@gmail.com>
2026-03-11 15:44:38 +0100

    Create page.tsx

---
+++ app/versand/page.tsx
@@ -0,0 +1,49 @@
+Versand & Lieferung
+
+Wir liefern schnell und zuverlässig in die ganze Schweiz und Liechtenstein.
+
+Versandgebiet
+
+Der Versand erfolgt ausschliesslich innerhalb der Schweiz und Liechtenstein.
+
+
+Lieferzeit
+
+Die Lieferung erfolgt in der Regel innerhalb von:
+
+2 – 5 Werktagen
+
+nach Bestelleingang.
+
+
+Versandkosten
+
+Die Versandkosten werden im Bestellprozess angezeigt.
+
+Ab einem Bestellwert von CHF 250.– liefern wir versandkostenfrei.
+
+
+Versandpartner
+
+Unsere Bestellungen werden je nach Produkt mit folgenden Versanddienstleistern versendet:
+
+• Die Post
+• DHL
+• DPD
+
+
+Sendungsverfolgung
+
+Sobald deine Bestellung versendet wurde, erhältst du eine Versandbestätigung per E-Mail mit einer Trackingnummer.
+
+
+Lieferverzögerungen
+
+In seltenen Fällen kann es aufgrund von hoher Nachfrage oder Lieferengpässen zu Verzögerungen kommen. Wir informieren dich selbstverständlich schnellstmöglich.
+
+
+Beschädigte Lieferung
+
+Sollte deine Bestellung beschädigt bei dir ankommen, kontaktiere bitte sofort unseren Support:
+
+support@iumatec.ch
